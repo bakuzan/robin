20e33db722eb06523f02caca3aca26a05ed4d6f7
bakuzan <imannoyed4days@hotmail.co.uk>
2019-05-11 19:29:43 +0100

    deps bump.

--- src/app/common/dashboard.service.ts
+++ src/app/common/dashboard.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { catchError, tap, map } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Urls } from 'src/app/common/constants';
